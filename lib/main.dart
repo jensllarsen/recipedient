9904d3bba36fa7113ee72c57fb2c4f113a4fadbb
Jens Larsen <dasuperman@gmail.com>
2019-05-04 12:41:21 -0700

    Add a button

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MainScreen());
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MainScreenState();
+  }
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List<String> _recipes = ['French Onion Soup'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("recipedient"),
-        ), // AppBar
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('resources/logo2.png'),
-              Text("recipedient"),
-            ],
-          ), // Column
-        ), // Card
-      ), // Scaffold
+          appBar: AppBar(
+            title: Text("recipedient"),
+          ), // AppBar
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  child: Text("Add recipe"),
+                  onPressed: () {},
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('resources/logo2.png'),
+                    Text("recipedient"),
+                  ], // Widget []
+                ), // Column
+              ), // Card
+            ], // []
+          ) // Card
+          ), // Scaffold
     ); // MaterialApp
   }
 }
