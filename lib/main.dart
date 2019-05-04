cc55b50f60d7f3c4d77053775b8e9d3c79662edd
Jens Larsen <dasuperman@gmail.com>
2019-05-04 14:32:57 -0700

    Switch to tab layout

--- lib/main.dart
+++ lib/main.dart
@@ -5,41 +5,29 @@ void main() => runApp(MainScreen());
 class MainScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _MainScreenState();
   }
 }
 
 class _MainScreenState extends State<MainScreen> {
-  List<String> _recipes = ['French Onion Soup'];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("recipedient"),
-          ), // AppBar
-          body: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  child: Text("Add recipe"),
-                  onPressed: () {},
+        home: DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              appBar: AppBar(
+                title: Text('recipedent'),
+                bottom: TabBar(
+                  tabs: [
+                    Tab(text: 'Search', icon: Icon(Icons.search)),
+                    Tab(text: 'Saved', icon: Icon(Icons.save)),
+                    Tab(text: 'Shopping', icon: Icon(Icons.shopping_cart)),
+                  ],
                 ),
               ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('resources/logo2.png'),
-                    Text("recipedient"),
-                  ], // Widget []
-                ), // Column
-              ), // Card
-            ], // []
-          ) // Card
-          ), // Scaffold
-    ); // MaterialApp
+            )
+        )
+    );
   }
 }
