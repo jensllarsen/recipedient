edbf4309e65b1e72c8f2e73677aaaa10b5fbf68e
Jens Larsen <dasuperman@gmail.com>
2019-05-05 15:21:50 -0700

    Update Recipe model class to fit all properties returned by API

--- lib/model/recipe.dart
+++ lib/model/recipe.dart
@@ -1,9 +1,109 @@
-class Recipe {
-  String label;
-  Uri imageUrl;
-  String source;
-  Uri recipeUrl;
-  int yield;
+// To parse this JSON data, do
+//
+//     final recipe = recipeFromJson(jsonString);
 
-  List<String> ingredients;
+import 'dart:convert';
+
+Recipe recipeFromJson(String str) => Recipe.fromMap(json.decode(str));
+
+String recipeToJson(Recipe data) => json.encode(data.toMap());
+
+class Recipe {
+  RecipeClass recipe;
+
+  Recipe({
+    this.recipe,
+  });
+
+  factory Recipe.fromMap(Map<String, dynamic> json) => new Recipe(
+        recipe: RecipeClass.fromMap(json["recipe"]),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "recipe": recipe.toMap(),
+      };
+}
+
+class RecipeClass {
+  String uri;
+  String label;
+  String image;
+  String source;
+  String url;
+  String shareAs;
+  int recipeYield;
+  List<String> dietLabels;
+  List<String> healthLabels;
+  List<dynamic> cautions;
+  List<String> ingredientLines;
+  List<Ingredient> ingredients;
+
+  RecipeClass({
+    this.uri,
+    this.label,
+    this.image,
+    this.source,
+    this.url,
+    this.shareAs,
+    this.recipeYield,
+    this.dietLabels,
+    this.healthLabels,
+    this.cautions,
+    this.ingredientLines,
+    this.ingredients,
+  });
+
+  factory RecipeClass.fromMap(Map<String, dynamic> json) => new RecipeClass(
+        uri: json["uri"],
+        label: json["label"],
+        image: json["image"],
+        source: json["source"],
+        url: json["url"],
+        shareAs: json["shareAs"],
+        recipeYield: json["yield"],
+        dietLabels: new List<String>.from(json["dietLabels"].map((x) => x)),
+        healthLabels: new List<String>.from(json["healthLabels"].map((x) => x)),
+        cautions: new List<dynamic>.from(json["cautions"].map((x) => x)),
+        ingredientLines:
+            new List<String>.from(json["ingredientLines"].map((x) => x)),
+        ingredients: new List<Ingredient>.from(
+            json["ingredients"].map((x) => Ingredient.fromMap(x))),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "uri": uri,
+        "label": label,
+        "image": image,
+        "source": source,
+        "url": url,
+        "shareAs": shareAs,
+        "yield": recipeYield,
+        "dietLabels": new List<dynamic>.from(dietLabels.map((x) => x)),
+        "healthLabels": new List<dynamic>.from(healthLabels.map((x) => x)),
+        "cautions": new List<dynamic>.from(cautions.map((x) => x)),
+        "ingredientLines":
+            new List<dynamic>.from(ingredientLines.map((x) => x)),
+        "ingredients":
+            new List<dynamic>.from(ingredients.map((x) => x.toMap())),
+      };
+}
+
+class Ingredient {
+  String text;
+  double weight;
+
+  Ingredient({
+    this.text,
+    this.weight,
+  });
+
+  factory Ingredient.fromMap(Map<String, dynamic> json) => new Ingredient(
+        text: json["text"],
+        weight: json["weight"].toDouble(),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "text": text,
+        "weight": weight,
+      };
 }
