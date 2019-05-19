8ec4cea3dd7a3e4c85d26cfcd64391ceaed5eafa
Jens Larsen <dasuperman@gmail.com>
2019-05-19 11:08:04 -0700

    Add status code check

--- lib/controller/edamamApi.dart
+++ lib/controller/edamamApi.dart
@@ -16,13 +16,18 @@ Future<List<Recipe>> getMatchingRecipes(String query) async {
     return createEmptyRecipeList();
   }
 
-  // Otherwise get the query and convert to Recipes.
+  // Otherwise get the query
   String request = '$edamamEndpoint?q=$query&app_id=$app_id&app_key=$app_key';
   try {
     response = await http.get(request);
+    // check the status code
+    if (response.statusCode == 200) {
+      return getRecipesFromJson(response.body);
+    } else {
+      throw Exception("Falied to retrive recipes! Status: ${response.statusCode}");
+    }
   } on Exception catch (e) {
     print("Exception trying to query recipes: ${e.toString()}");
     return createEmptyRecipeList();
   }
-  return getRecipesFromJson(response.body);
 }
