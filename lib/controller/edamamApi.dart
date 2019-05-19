9f1f45ae4ef951d52dbcf0be0c8293636eb5d403
Jens Larsen <dasuperman@gmail.com>
2019-05-18 19:59:38 -0700

    renamed endpoint var to be clearer

--- lib/controller/edamamApi.dart
+++ lib/controller/edamamApi.dart
@@ -5,7 +5,7 @@ import 'package:recipedient/controller/secrets.dart';
 
 import 'package:recipedient/model/recipe.dart';
 
-final String endpoint = "https://api.edamam.com/search";
+final String edamamEndpoint = "https://api.edamam.com/search";
 
 // Queries the Edamam API with [query] and returns a List of Recipe objects
 //
@@ -17,7 +17,7 @@ Future<List<Recipe>> getMatchingRecipes(String query) async {
   }
 
   // Otherwise get the query and convert to Recipes.
-  String request = '$endpoint?q=$query&app_id=$app_id&app_key=$app_key';
+  String request = '$edamamEndpoint?q=$query&app_id=$app_id&app_key=$app_key';
   try {
     response = await http.get(request);
   } on Exception catch (e) {
