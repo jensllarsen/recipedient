e2d7367b0f878f4d5078303f68ca8ccd85bb9dd3
Jens Larsen <dasuperman@gmail.com>
2019-05-28 07:40:14 -0700

    Make About screen navigation work

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:recipedient/view/about.dart';
+import 'package:recipedient/view/saved.dart';
 
 /// Import navigation screens
 import 'package:recipedient/view/search.dart';
 import 'package:recipedient/view/shopping.dart';
-import 'package:recipedient/view/saved.dart';
 
 const int _NUM_TABS = 3;
 
@@ -13,43 +13,49 @@ void main() => runApp(MainScreen());
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: _NUM_TABS,
-        child: Scaffold(
-          backgroundColor: Colors.deepOrange,
-          appBar: AppBar(
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.info),
-                onPressed: () {
-                  // TODO: Fix this about screen navigation
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AboutScreen(),
-                    ),
-                  );
-                },
-              )
+    return MaterialApp(home: HomeScreenDisplay());
+  }
+}
+
+class HomeScreenDisplay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return DefaultTabController(
+      length: _NUM_TABS,
+      child: Scaffold(
+        backgroundColor: Colors.deepOrange,
+        appBar: AppBar(
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.info),
+              onPressed: () {
+                // TODO: Fix this about screen navigation
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AboutScreen(),
+                  ),
+                );
+              },
+            )
+          ],
+          backgroundColor: Colors.green[600],
+          title: Text('Recipedient'),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Search', icon: Icon(Icons.search)),
+              Tab(text: 'Saved', icon: Icon(Icons.save)),
+              Tab(text: 'Shopping', icon: Icon(Icons.shopping_cart)),
             ],
-            backgroundColor: Colors.green[600],
-            title: Text('Recipedient'),
-            bottom: TabBar(
-              tabs: [
-                Tab(text: 'Search', icon: Icon(Icons.search)),
-                Tab(text: 'Saved', icon: Icon(Icons.save)),
-                Tab(text: 'Shopping', icon: Icon(Icons.shopping_cart)),
-              ],
-            ),
           ),
-          body: TabBarView(
-            children: [
-              SearchScreen(),
-              SavedScreen(),
-              ShoppingScreen(),
-            ],
-          ),
+        ),
+        body: TabBarView(
+          children: [
+            SearchScreen(),
+            SavedScreen(),
+            ShoppingScreen(),
+          ],
         ),
       ),
     );
