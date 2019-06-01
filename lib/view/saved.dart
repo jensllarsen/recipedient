83739f6a5ffd29f6c647e0af08a99571fe4e53da
Jens Larsen <dasuperman@gmail.com>
2019-06-01 12:27:45 -0700

    Add Saved screen structure

--- lib/view/saved.dart
+++ lib/view/saved.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:recipedient/widgets/color_palette.dart';
 
 class SavedScreen extends StatefulWidget {
   @override
@@ -8,9 +9,37 @@ class SavedScreen extends StatefulWidget {
 }
 
 class _SavedScreenState extends State<SavedScreen> {
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Container();
+    TextStyle titleStyle = Theme.of(context).textTheme.subhead;
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int item) {
+        return Card(
+          color: Colors.white,
+          elevation: 2,
+          child: ListTile(
+            onTap: () {
+              debugPrint('List Item clicked!');
+            },
+            title: Text(
+              'Dummy title',
+              style: titleStyle,
+            ),
+            subtitle: Text('Dummy data'),
+            leading: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Image.asset('resources/bg.png'),
+            ),
+            trailing: Icon(
+              Icons.delete,
+              color: accentColor,
+            ),
+          ),
+        );
+      },
+    );
   }
 }
