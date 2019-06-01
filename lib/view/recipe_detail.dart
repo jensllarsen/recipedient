6afefa7090d1aa9b24fa07c348003153e81a6838
Jens Larsen <dasuperman@gmail.com>
2019-06-01 13:59:29 -0700

    Update style and add button to launch recipe website

--- lib/view/recipe_detail.dart
+++ lib/view/recipe_detail.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:recipedient/model/recipe.dart';
 import 'package:recipedient/widgets/color_palette.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// Display the details of the [recipe] on a new screen
 ///
@@ -21,16 +22,14 @@ class RecipeDetail extends StatelessWidget {
           padding: EdgeInsets.all(20),
           child: Column(
             children: <Widget>[
-              Card(
-                elevation: 20,
-                child: Image.network(recipe.image),
+              Expanded(
+                child: Card(
+                  elevation: 20,
+                  child: Image.network(recipe.image),
+                ),
               ),
               Container(
                 height: 20,
-              ),
-              Text(
-                recipe.source,
-                style: TextStyle(fontStyle: FontStyle.italic),
               ),
               Container(
                 height: 20,
@@ -43,17 +42,25 @@ class RecipeDetail extends StatelessWidget {
                   },
                 ),
               ),
-              Text(recipe.url),
-              MaterialButton(
-                color: accentColor,
-                onPressed: () {},
-                child: Text("Save"),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: MaterialButton(
+                  child: Text("Open recipe on ${recipe.source}"),
+                  color: accentColor,
+                  onPressed: () async {
+                    if (await canLaunch(recipe.url)) {
+                      await launch(recipe.url);
+                    } else {
+                      throw 'Could not launch $recipe.url';
+                    }
+                  },
+                ),
               ),
               MaterialButton(
                 color: accentColor,
                 onPressed: () {},
-                child: Text("Open URL"),
-              )
+                child: Text("Save Recipe"),
+              ),
             ],
           ),
         ),
