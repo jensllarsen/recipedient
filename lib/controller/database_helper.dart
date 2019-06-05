a3c2599fcad650cd8b493227a8e00cae0dffeada
Jens Larsen <dasuperman@gmail.com>
2019-06-05 12:44:18 -0700

    Add Ingredient table into database

--- lib/controller/database_helper.dart
+++ lib/controller/database_helper.dart
@@ -9,22 +9,24 @@ import 'package:sqflite/sqflite.dart';
 /// [DatabaseHelper] is a singleton
 ///
 class DatabaseHelper {
-  final String dbName = 'recipedient.db';
-  final int dbVersion = 1;
+  static const String _DB_NAME = 'recipedient.db';
+  static const int _DB_VERSION = 1;
 
-  final String recipeTableName = 'recipe_table';
-  final String colId = 'id';
-  final String colUri = 'uri';
-  final String colLabel = 'label';
-  final String colImage = 'image';
-  final String colSource = 'source';
-  final String colUrl = 'url';
-  final String colShareAs = 'shareAs';
-  final String colRecipeYield = 'recipeYield';
-  final String colDietLabels = 'dietLabels';
-  final String colHealthLabels = 'colHealthLables';
-  final String colCautions = 'cautions';
-  final String colIngredientLines = 'ingredientLines';
+  static const String RECIPE_TABLE_NAME = 'Recipe';
+  static const String RECIPE_COL_ID = 'id';
+  static const String RECIPE_COL_URI = 'uri';
+  static const String RECIPE_COL_LABEL = 'label';
+  static const String RECIPE_COL_IMAGE = 'image';
+  static const String RECIPE_COL_SOURCE = 'source';
+  static const String RECIPE_COL_SOURCE_URL = 'url';
+  static const String RECIPE_COL_SHARE_AS = 'shareAs';
+  static const String RECIPE_COL_RECIPE_YIELD = 'recipeYield';
+  // TODO: Add date added field
+
+  static const String INGREDIENT_TABLE_NAME = 'Ingredient';
+  static const String INGREDIENT_COL_ID = 'id';
+  static const String INGREDIENT_ITEM = 'item';
+  static const String INGREDIENT_COL_RECIPE_ID = 'recipeId';
 
   DatabaseHelper._createInstance();
 
@@ -56,19 +58,23 @@ class DatabaseHelper {
   /// Runs the CREATE TABLE command
   ///
   void _createDb(Database db, int newVersion) async {
-    await db.execute('CREATE TABLE $recipeTableName ('
-        '$colId INTEGER PRIMARY KEY AUTOINCREMENT,'
-        '$colUri TEXT,'
-        '$colLabel TEXT,'
-        '$colImage TEXT,'
-        '$colSource TEXT,'
-        '$colUrl TEXT,'
-        '$colShareAs TEXT,'
-        '$colRecipeYield REAL,'
-        '$colDietLabels TEXT,'
-        '$colHealthLabels TEXT,'
-        '$colCautions TEXT,'
-        '$colIngredientLines TEXT'
+    await db.execute('CREATE TABLE $RECIPE_TABLE_NAME ('
+        '$RECIPE_COL_ID INTEGER PRIMARY KEY AUTOINCREMENT,'
+        '$RECIPE_COL_URI TEXT,'
+        '$RECIPE_COL_LABEL TEXT,'
+        '$RECIPE_COL_IMAGE TEXT,'
+        '$RECIPE_COL_SOURCE TEXT,'
+        '$RECIPE_COL_SOURCE_URL TEXT,'
+        '$RECIPE_COL_SHARE_AS TEXT,'
+        '$RECIPE_COL_RECIPE_YIELD REAL'
+        ');');
+
+    await db.execute('CREATE TABLE $INGREDIENT_TABLE_NAME ('
+        '$INGREDIENT_COL_ID INTEGER PRIMARY KEY AUTOINCREMENT,'
+        '$INGREDIENT_ITEM TEXT,'
+        '$INGREDIENT_COL_RECIPE_ID INTEGER,'
+        'FOREIGN KEY ($INGREDIENT_COL_RECIPE_ID) '
+        'REFERENCES $RECIPE_TABLE_NAME($RECIPE_COL_ID)'
         ');');
   }
 
@@ -76,45 +82,89 @@ class DatabaseHelper {
   ///
   Future<Database> initializeDb() async {
     Directory directory = await getApplicationDocumentsDirectory();
-    String dbPath = directory.path + dbName;
+    String dbPath = directory.path + _DB_NAME;
 
-    Database recipeDb =
-        await openDatabase(dbPath, version: dbVersion, onCreate: _createDb);
-    return recipeDb;
+    try {
+      Database recipeDb =
+          await openDatabase(dbPath, version: _DB_VERSION, onCreate: _createDb);
+      return recipeDb;
+    } catch (e) {
+      print('Unable to initialize database: $e');
+    }
+    return null;
   }
 
-  /// Retrieve operation. Get all Recipe objects from the database
+  /// Retrieve all recipes operation. Get all Recipe objects from the database
   ///
   Future<List<Map<String, dynamic>>> getRecipeMapList() async {
     Database db = await this.database;
-    List<Map<String, dynamic>> result = await db.query(recipeTableName);
+    List<Map<String, dynamic>> result = await db.query(RECIPE_TABLE_NAME);
     return result;
   }
 
-  /// Create operation. Insert a Recipe object, [recipe], into the database
+  /// Retrieve ingredients operation. Retrieve all ingredients for a Recipe object.
+  ///
+  Future<List<String>> getIngredients(int id) async {
+    // TODO: Fix this
+    Database db = await this.database;
+    List<Map<String, String>> result = await db.query(INGREDIENT_TABLE_NAME,
+        where: '$RECIPE_COL_ID=?', whereArgs: [id]);
+
+    List<String> ingredients;
+    for (int index = 0; index < result.length; index++) {
+      Map<String, String> ingredient = result[index];
+      ingredients.add(ingredient['item']);
+    }
+    return ingredients;
+  }
+
+  /// Create recipe operation. Insert a Recipe object, [recipe], into the database
   /// Returns the inserted object ID
   ///
   Future<int> insertRecipe(Recipe recipe) async {
     Database db = await this.database;
-    int result = await db.insert(recipeTableName, recipe.toJson());
+    int result = await db.insert(RECIPE_TABLE_NAME, recipe.toJson());
     return result;
   }
 
-  /// Update operation. Update a Recipe object.
+  /// Create ingredient operation. Insert an ingredient, [ingredient], into the database
+  /// Returns the inserted object ID
+  ///
+  Future<int> insertIngredient(String ingredientString) async {
+    Database db = await this.database;
+
+    Map<String, dynamic> ingredient = {
+      DatabaseHelper.INGREDIENT_ITEM: ingredientString
+    };
+
+    int result = await db.insert(INGREDIENT_TABLE_NAME, ingredient);
+    return result;
+  }
+
+  /// Update recipe operation. Update a Recipe object.
   ///
   Future<int> updateRecipe(Recipe recipe) async {
     Database db = await this.database;
-    int result = await db.update(recipeTableName, recipe.toJson(),
-        where: '$colId=?', whereArgs: [recipe.id]);
+    int result = await db.update(RECIPE_TABLE_NAME, recipe.toJson(),
+        where: '$RECIPE_COL_ID=?', whereArgs: [recipe.id]);
     return result;
   }
 
-  /// Delete operation. Delete a Recipe object from the database
+  /// Delete recipe operation. Delete a Recipe object from the database
   ///
   Future<int> deleteRecipe(int id) async {
     Database db = await this.database;
-    int result =
-        await db.rawDelete('DELETE FROM $recipeTableName WHERE $colId=$id');
+    int result = await db
+        .rawDelete('DELETE FROM $RECIPE_TABLE_NAME WHERE $RECIPE_COL_ID=$id');
+    return result;
+  }
+
+  /// Delete ingredient operation. Delete an ingredient from the database
+  ///
+  Future<int> deleteIngredient(int id) async {
+    Database db = await this.database;
+    int result = await db.rawDelete(
+        'DELETE FROM $INGREDIENT_TABLE_NAME WHERE $INGREDIENT_COL_ID=$id');
     return result;
   }
 
@@ -124,7 +174,7 @@ class DatabaseHelper {
     Database db = await this.database;
 
     List<Map<String, dynamic>> x =
-        await db.rawQuery('SELECT COUNT (*) FROM $recipeTableName');
+        await db.rawQuery('SELECT COUNT (*) FROM $RECIPE_TABLE_NAME');
     int result = Sqflite.firstIntValue(x);
     return result;
   }
