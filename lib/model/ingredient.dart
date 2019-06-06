11f5d2333967db7ba3b8e3bdb4ad9cc3bfcc0f1c
Jens Larsen <dasuperman@gmail.com>
2019-06-06 07:19:39 -0700

    Add Ingredient model class

--- lib/model/ingredient.dart
+++ lib/model/ingredient.dart
@@ -1,9 +1,31 @@
-class Ingredient {
-  String text;
-  double weight;
+import 'package:json_annotation/json_annotation.dart';
 
-  Ingredient({
-    this.text,
-    this.weight,
-  });
+part 'ingredient.g.dart';
+
+/// Plain class to store Ingredient data.
+///
+/// Utilizes json_serializable to create to and from JSON methods in
+/// ingredient.g.dart.
+///
+@JsonSerializable()
+class Ingredient {
+  int id;
+  String item;
+  int recipeId;
+
+  Ingredient(
+    this.item,
+    this.recipeId,
+  );
+
+  Ingredient.withId(
+    this.id,
+    this.item,
+    this.recipeId,
+  );
+
+  factory Ingredient.fromJson(Map<String, dynamic> json) =>
+      _$IngredientFromJson(json);
+
+  Map<String, dynamic> toJson() => _$IngredientToJson(this);
 }
