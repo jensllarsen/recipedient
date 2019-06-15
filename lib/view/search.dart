3293d69428a8b0b5cd9fc533d82fd8d81055346b
Jens Larsen <dasuperman@gmail.com>
2019-06-15 11:10:38 -0700

    Add snackbar to display searching

--- lib/view/search.dart
+++ lib/view/search.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recipedient/widgets/color_palette.dart';
+import 'package:recipedient/widgets/display_snack_bar.dart';
 import 'package:recipedient/widgets/recipe_list.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -22,37 +23,42 @@ class _SearchScreenState extends State<SearchScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: TextField(
-            cursorColor: accentColor,
-            decoration: InputDecoration(hintText: 'Ingredients'),
-            controller: searchController,
-            onSubmitted: (String value) {
-              setState(() {
-                query = searchController.text.toString();
-              });
-              return searchController.text.toString();
-            },
-          ),
-        ),
-        RaisedButton(
-          color: accentColor,
-          child: Text(
-            "Search",
-            style: TextStyle(color: textPrimaryColor),
-          ),
-          onPressed: () {
-            print("Search: ${searchController.text.toString()}");
-            setState(() {
-              query = searchController.text.toString();
-            });
-          },
-        ),
-        RecipeList(query),
-      ],
+    return Builder(
+      builder: (context) {
+        return Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                cursorColor: accentColor,
+                decoration: InputDecoration(hintText: 'Ingredients'),
+                controller: searchController,
+                onSubmitted: (String value) {
+                  setState(() {
+                    query = searchController.text.toString();
+                  });
+                  return searchController.text.toString();
+                },
+              ),
+            ),
+            RaisedButton(
+              color: accentColor,
+              child: Text(
+                "Search",
+                style: TextStyle(color: textPrimaryColor),
+              ),
+              onPressed: () {
+                displaySnackBar(
+                    context, "Searching: ${searchController.text.toString()}");
+                setState(() {
+                  query = searchController.text.toString();
+                });
+              },
+            ),
+            RecipeList(query),
+          ],
+        );
+      },
     );
   }
 }
