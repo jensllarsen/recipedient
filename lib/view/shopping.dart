03a75be94cd20507d8e1d5e0bd4f42cb88d54b3e
Jens Larsen <dasuperman@gmail.com>
2019-06-16 07:11:06 -0700

    Add skeleton of Shopping view

--- lib/view/shopping.dart
+++ lib/view/shopping.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:recipedient/controller/database_helper.dart';
 
 class ShoppingScreen extends StatefulWidget {
   @override
@@ -8,9 +9,32 @@ class ShoppingScreen extends StatefulWidget {
 }
 
 class _ShoppingScreenState extends State<ShoppingScreen> {
+  DatabaseHelper databaseHelper = new DatabaseHelper();
+
+  List<String> shopping;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: FutureBuilder<List<String>>(
+          future: databaseHelper.getShoppingList(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Container();
+            }
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data[index]),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 }
